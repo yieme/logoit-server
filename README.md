72837c9943903d2c906c89d11effc770f80d4d71
yieme <yieme@cormea.com>
2015-02-06 09:28:37 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# logoit-server
+# logoit-server <img src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" align="right" height="120">
 LogoIt Server
+
+# Install
+
+
+```js
+npm install logoit-server --save
+```
+
+# License: MIT
---
+++ index.js
@@ -0,0 +1,17 @@
+// logoit-server - LogoIt Server - Copyright (C) 2015 by yieme - All Rights Reserved - License: MIT
+
+;(function() {
+  'use strict';
+
+  // var convar = require('convar')
+
+  var LogoitServer = function() {
+
+  }
+
+
+
+  if (typeof exports === 'object') module.exports = LogoitServer // support CommonJS
+  // else if (typeof define === 'function' && define.amd) define(function() { return LogoitServer }) // support AMD
+  // else this.LogoitServer = LogoitServer // support browser
+})();
